9c2d1a42db10a0b63679c4226be40f835dc4e3d3
Joy Kirii <joykirii@gmail.com>
2022-03-11 07:33:27 -0800

    print alaphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
-/*
- * main - alphabet in lowecase, followed by a new line
- *
- * Description: The numbers are generated automatically
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int ch;
-
+	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
-
 	return (0);
 }
