3bc6b07ccc942b10a140ef8826e138fd23948a63
Joy Kirii <joykirii@gmail.com>
2022-03-11 08:30:22 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ * followed by a new line
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
