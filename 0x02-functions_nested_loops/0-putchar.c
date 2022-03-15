6a1790d202c536bf9e3c6864509c94c3d31180ff
Joy Kirii <joykirii@gmail.com>
2022-03-15 06:46:25 -0700

    prints _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main - print _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *s = "_putchar";
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
