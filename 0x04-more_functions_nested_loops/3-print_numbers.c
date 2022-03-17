7aa361b386755d98f98ff716378cd25c95af8c17
Joy Kirii <joykirii@gmail.com>
2022-03-17 06:12:51 -0700

    print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ * Return: numbers 0 - 9
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
