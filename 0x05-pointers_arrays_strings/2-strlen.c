fac893a267c2a1fd561c81457404a48ecda3235b
Wakarende <joykirii@gmail.com>
2022-03-21 14:22:05 +0000

    create function that returns a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Finds the length of a strings
+ * @s: character which length is to be found.
+ *
+ * Return: none
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
