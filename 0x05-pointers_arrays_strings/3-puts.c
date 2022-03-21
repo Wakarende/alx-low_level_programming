696e4c3bdf716ea2a41ac2b04ae30d553fbc2ad7
Wakarende <joykirii@gmail.com>
2022-03-21 14:39:42 +0000

    function that prints a string followed by a new line to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout
+ * @str: string to be printed out
+ *
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,10 @@
 #include <unistd.h>
 #include <math.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *s);
+
 #endif /* MAIN_H */
