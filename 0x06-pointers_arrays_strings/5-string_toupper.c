d1f1d400f53070b8b4a413695d79e134ce134730
Wakarende <joykirii@gmail.com>
2022-03-24 14:30:15 +0000

    capitalize

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes lowercase letters to uppercase
+ * @str: character to be changed
+ *
+ * Return: Uppercse letters
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
