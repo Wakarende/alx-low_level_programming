6e3fc924083b78a227bb308d0ef13fcac8a16f3b
Wakarende <joykirii@gmail.com>
2022-03-28 13:47:52 +0000

    function that searches for a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string
+ * @c: character to be located
+ *
+ * Return: character or NULL if character is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
+
+/**
+ * *_strpbrk - searched a string for any set of bytes
+ * @s: string to be searched
+ * @accept: string to be compared
+ *
+ * Return: pointer to the byte in s that matches one in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if ((s == NULL) && (accept == NULL))
+	{
+		return (NULL);
+	}
+
+	while (*s)
+	{
+		if (_strchr(accept, *s))
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return (NULL);
+}
