0e1fe5a247b48defee52ac2555c5ae07e7f8ee46
Wakarende <joykirii@gmail.com>
2022-03-30 07:49:18 +0000

    recursive printing of string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif /* MAIN_H */
